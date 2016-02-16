9d9f0e561054515f5d1c3ca0daaf56e8f4d996ee
SensShayne <sorkrywkd@gmail.com>
2016-02-16 09:09:16 +0900

    캐릭터 애니메이션 FMS는 얼추 잘 됨 이펙트 몇가지를 프리팹으로 만듬

---
+++ Assets/Gunster/Scripts/Character.cs
@@ -0,0 +1,237 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum CharacterPose
+{
+	STAND = 0,
+	CROUCH = 1,
+	CROWL = 2,
+	SKY = 3
+}
+
+public enum DeathDirection
+{
+	BACK = -1,
+	FRONT = 1,
+}
+
+public enum MoveDirection
+{
+	BACKWARD = -1,
+	STOP = 0,
+	FORWARD = 1
+}
+
+[RequireComponent(typeof (Animator))]
+[RequireComponent(typeof (Rigidbody2D))]
+public class Character : MonoBehaviour 
+{
+	[SerializeField] float standSpeed;
+	[SerializeField] float crouchSpeed;
+	[SerializeField] float crowlSpeed;
+	[SerializeField] float skyMoveSpeed;
+	[SerializeField] float boosterSpeed;
+
+	[SerializeField] LayerMask _whatIsGround;
+
+	Transform _skyCheck;    
+	const float _skyCheckRadius = 0.05f;
+
+	Animator _animator;
+	Rigidbody2D _rigidbody;
+
+	MoveDirection _moveDirection = MoveDirection.STOP;
+	CharacterPose _pose = CharacterPose.STAND;
+
+	bool _flip = false;
+
+	bool _death = false;
+
+
+	// unity functions ---------------------------------------------------
+	void Start () 
+	{
+		_skyCheck = transform.Find("Sky Check");
+		_animator = GetComponent<Animator> ();
+		_rigidbody = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate () 
+	{
+		if (_death)
+		{
+			if (IsSkying ())
+			{
+				SetPose (CharacterPose.SKY);
+			}
+			else
+			{
+				SetPose (CharacterPose.STAND);
+			}
+		}
+		// test
+		else
+		{
+			if (Input.GetKeyDown (KeyCode.Q))
+			{
+				Death (DeathDirection.BACK);
+			}
+			else if (Input.GetKeyDown (KeyCode.E))
+			{
+				Death (DeathDirection.FRONT);
+			}
+		}
+	}
+
+	// public functions ---------------------------------------------------
+	public void Move (float horizontal, float vertical, float horizontalRaw, float verticalRaw)
+	{
+		if (_death)
+		{
+			return;
+		}
+
+		// move
+		Vector2 position = _rigidbody.position;
+		if (_pose == CharacterPose.SKY) 
+		{
+			position.x += horizontal * MoveSpeed();
+		} 
+		else 
+		{
+			position.x += horizontalRaw * MoveSpeed();
+		}
+		_rigidbody.position = position;
+
+		Vector2 velocity = _rigidbody.velocity;
+		if (vertical > 0)
+		{
+			velocity.y = boosterSpeed;
+		}
+		_rigidbody.velocity = velocity;
+
+
+		// pose
+		if (IsSkying ())
+		{
+			SetPose (CharacterPose.SKY);
+		}
+		else
+		{
+			if (-0.5f <= vertical && vertical < 0.0f)
+			{
+				SetPose (CharacterPose.CROUCH);
+			}
+			else if (-1.0f <= vertical && vertical < -0.5f)
+			{
+				SetPose (CharacterPose.CROWL);
+			}
+			else if (vertical == 0.0f)
+			{
+				SetPose (CharacterPose.STAND);
+			}
+		}
+
+
+		// direction
+		if (horizontalRaw == 0)
+		{
+			SetMoveDirection (MoveDirection.STOP);
+		}
+		else 
+		{
+			SetMoveDirection (MoveDirection.FORWARD);
+
+			if ((horizontal > 0 && !_flip) || 
+				(horizontal < 0 && _flip))
+			{
+				Flip ();
+			}
+		}                                                                                             
+	}
+
+	public void Shoot (Vector3 aimPosition)
+	{
+		if (_death)
+		{
+			return;
+		}
+	}
+
+
+	// private functions --------------------------------------------------
+	float MoveSpeed()
+	{
+		switch (_pose) 
+		{
+			case CharacterPose.STAND:
+				{
+					return standSpeed;
+				}
+			case CharacterPose.CROUCH:
+				{
+					return crouchSpeed;
+				}
+			case CharacterPose.CROWL:
+				{
+					return crowlSpeed;
+				}
+			case CharacterPose.SKY:
+				{
+					return skyMoveSpeed;
+				}
+			default:
+				{
+					return 0;
+				}
+		}
+	}
+
+	void SetPose (CharacterPose pose)
+	{
+		_pose = pose;
+
+		_animator.SetInteger ("pose", (int)pose);
+	}
+
+	void SetMoveDirection (MoveDirection moveDirection)
+	{
+		_moveDirection = moveDirection;
+
+		_animator.SetInteger ("move", (int)moveDirection);
+	}
+
+	void Death (DeathDirection deathDirection)
+	{
+		_animator.SetInteger ("death", (int)deathDirection);
+
+		_animator.SetTrigger ("deathTrigger");
+
+		_death = true;
+	}
+
+	void Flip()
+	{
+		_flip = !_flip;
+
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	bool IsSkying()
+	{
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(_skyCheck.position, _skyCheckRadius, _whatIsGround);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	// coroutine ----------------------------------------------------------
+}
